d793dc89c9f8596bb17f93d7467005f3ce26f209
Pelle Wessman <pelle@kodfabrik.se>
2015-07-21 13:22:53 +0200

    fix(main): support the "bookmark-of" as well
    The "bookmark-of" property is the one pushed for by http://indiewebcamp.com/bookmark while Micropub for some reason pushes for the use of a plain "bookmark" property instead: http://indiewebcamp.com/micropub#New_Bookmark
    
    As the micropub properties should mirror the microformat properties we should at least support both.

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ Formatter.prototype.preFormat = function (data) {
   if (!_.isEmpty(data.properties['in-reply-to']) || !_.isEmpty(data.properties['like-of'])) {
     data.derived.category = 'interaction';
   }
-  if (!_.isEmpty(data.properties.bookmark)) {
+  if (!_.isEmpty(data.properties.bookmark) || !_.isEmpty(data.properties['bookmark-of'])) {
     data.derived.category = 'bookmark';
   }
 
